added5ab6985e6859aed350b1c1963af02643b35
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2025-10-15 18:12:22 +0300

    add json.hpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include "./json.hpp"
 
-using std::wcout;
 using std::cout;
 using std::endl;
+using std::wcout;
+using json = nlohmann::json;
 
 int main()
 {
-	std::string newString = "testString";
-	cout << newString;
+	std::fstream f("students.json");
+	json jsonData = json::parse(f);
+	cout << jsonData;
 }
