0463ffe7be4401541e3361572ee7b9a98d2bc9d5
Ivan Borodkin <borodkin.ivan2003@gmail.com>
2025-10-16 14:27:08 +0300

    start

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -12,6 +12,26 @@ using json = nlohmann::json;
 
 class JsonFile
 {
+private:
+	const std::string filename;
+	json content;
+
+public:
+	JsonFile(std::string f) : filename(f)
+	{
+		std::fstream fileContent(f);
+		content = json::parse(fileContent);
+	}
+
+	json getRawJson()
+	{
+		return content;
+	}
+
+	std::string getJson()
+	{
+		return content.dump();
+	}
 };
 
 int main()
